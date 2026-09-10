92ec078121d1af28077de20c09a85c9b997e9330
Dian Pranawan <dianpranaw@gmail.com>
2026-09-10 12:44:12 +0700

    Bagian 1

---
+++ app.js
@@ -0,0 +1,29 @@
+function calculateDiscountedPrice(price, discountPercent) {
+    return price - (price * discountPercent) / 100;
+}
+
+console.log(calculateDiscountedPrice(100000, 10));
+
+const cart = [
+    { title: "Laptop", price: 1000, discountPercent: 10 },
+    { title: "Mouse", price: 20, discountPercent: 5 },
+    { title: "Keyboard", price: 50, discountPercent: 0 }
+];
+
+function applyDiscounts(cart) {
+    const result = [];
+
+    for (const item of cart) {
+        const discountedPrice =
+            item.price - (item.price * item.discountPercent) / 100;
+
+        result.push({
+            title: item.title,
+            price: discountedPrice
+        });
+    }
+
+    return result;
+}
+
+console.log(applyDiscounts(cart));
