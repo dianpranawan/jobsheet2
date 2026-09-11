6d4da0409e0385834e8878cb4cbd65427b949c95
Dian Pranawan <dianpranaw@gmail.com>
2026-09-11 20:03:51 +0700

    Bagian 7

--- app.js
+++ app.js
@@ -232,3 +232,60 @@ function linearSearchProductById(products, targetId) {
 }
 
 console.log(linearSearchProductById(products, 2));
+
+// Latihan 7.1
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] === target) {
+            return mid;
+        }
+
+        if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+const sortedNumbers = [10, 20, 30, 40, 50];
+
+console.log(binarySearch(sortedNumbers, 40));
+console.log(binarySearch(sortedNumbers, 25));
+
+
+// Latihan 7.2
+const sortedProducts = [...nestedProducts].sort(
+    (a, b) => a.price - b.price
+);
+
+function binarySearchByPrice(sortedProducts, targetPrice) {
+    let left = 0;
+    let right = sortedProducts.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (sortedProducts[mid].price === targetPrice) {
+            return mid;
+        }
+
+        if (sortedProducts[mid].price < targetPrice) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+console.log(sortedProducts);
+console.log(binarySearchByPrice(sortedProducts, 1200));
