1de27b8330f4ddd352a2a7f56220a0e26d3d929c
Dian Pranawan <dianpranaw@gmail.com>
2026-09-12 20:44:01 +0700

    Bagian 9

--- app.js
+++ app.js
@@ -339,3 +339,32 @@ console.log(sortProducts(nestedProducts, "price-asc"));
 console.log(sortProducts(nestedProducts, "price-desc"));
 console.log(sortProducts(nestedProducts, "rating"));
 console.log(sortProducts(nestedProducts, "title"));
+
+// Latihan 9.1
+function groupByCategory(products) {
+    return products.reduce((groups, product) => {
+        const key = product.category;
+
+        if (!groups[key]) {
+            groups[key] = [];
+        }
+
+        groups[key].push(product);
+
+        return groups;
+    }, {});
+}
+
+const groupedProducts = groupByCategory(nestedProducts);
+
+console.log(groupedProducts);
+
+// Latihan 9.2
+const categorySummary = Object.entries(groupedProducts).map(
+    ([category, products]) => ({
+        category,
+        totalProducts: products.length
+    })
+);
+
+console.log(categorySummary);
