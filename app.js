5870dcb3b1d2d1d6156c3136dce25818d32d0f5a
Dian Pranawan <dianpranaw@gmail.com>
2026-09-14 08:45:08 +0700

    Bagian 19

--- app.js
+++ app.js
@@ -610,3 +610,25 @@ function render() {
 }
 
 render();
+
+// Latihan 19.1
+const searchInput = document.querySelector("#search-input");
+
+searchInput.addEventListener("input", (e) => {
+    state.search = e.target.value;
+    render();
+});
+
+const categorySelect = document.querySelector("#category-select");
+
+categorySelect.addEventListener("change", (e) => {
+    state.category = e.target.value;
+    render();
+});
+
+const sortSelect = document.querySelector("#sort-select");
+
+sortSelect.addEventListener("change", (e) => {
+    state.sortBy = e.target.value;
+    render();
+});
